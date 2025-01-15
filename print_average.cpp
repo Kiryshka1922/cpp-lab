16107b922d57e60ab7c5ceee7732b921c4f02d89
Kiryshka1922 <kipabelousov@edu.hse.ru>
2025-01-15 19:57:50 +0300

    Update print_average.cpp

--- print_average.cpp
+++ print_average.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+double Transform (char mark){
+     switch (mark)
+     {
+     case 'A':
+          return 5;
+          break;
+
+     case 'B':
+          return 4;
+          break;
+
+     case 'C':
+     case 'D':
+          return 3;
+          break;
+
+     case 'F':
+          return 2;
+          break;
+     default:
+          break;
+     }
+}
 // Шаблонная функция для вычисления и вывода средней оценки (для трех оценок)
 template <typename T>
 void print_average_mark(const char *name, T mark1, T mark2, T mark3)
@@ -9,7 +32,7 @@ void print_average_mark(const char *name, T mark1, T mark2, T mark3)
      std::cout << name << "'s average mark: " << std::fixed << std::setprecision(5) << (mark1 + mark2 + mark3) / 3.0 << std::endl;
 }
 
-// Перегруженная функция для вычисления и вывода средней оценки (для двух оценок
+// Перегруженная функция для вычисления и вывода средней оценки (для двух оценок)
 template <typename T>
 void print_average_mark(const char *name, T mark1, T mark2)
 {
@@ -20,9 +43,7 @@ void print_average_mark(const char *name, T mark1, T mark2)
 //ЯВНАЯ ПЕРЕГРУЗКА обобщенной функции
 void print_average_mark(const char *name, char mark1, char mark2, char mark3)
 {
-     double value1 = mark1 - 64, value2 = mark2 - 64, value3 = mark3 - 64; // Преобразование букв в числовое соответствуещее представление, по таблице ASCI
-     std::cout << name << "'s average mark (American grading system): " << std::fixed << std::setprecision(5) << ((value1) + (value2) + (value3)) / 3
-    << std::endl;
+     std::cout << name << "'s average mark (American grading system): " << std::fixed << std::setprecision(5) << (Transform(mark1) + Transform(mark2) + Transform(mark3)) / 3 << std::endl;
 }
 
 int main()
@@ -30,7 +51,7 @@ int main()
      print_average_mark("Ivan", 4, 4, 4);
      print_average_mark("Alex", 3, 3, 4);
      print_average_mark("Max", 3.5, 3.8, 3.655);
-     print_average_mark("Jack", 'B', 'B', 'B');
+     print_average_mark("Jack", 'A', 'F', 'B');
      print_average_mark("Jane", 4, 5);
      print_average_mark("John", 2.5, 3.5);
      return 0;
